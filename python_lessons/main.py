4c67e3b423e767531adfae6a312c0971cdaf26d3
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 05:14:14 +0300

    input function/usr/local/bin/python3.12

--- python_lessons/main.py
+++ python_lessons/main.py
@@ -1,6 +1,9 @@
+import random 
+
 def get_choices():
   player_choice = input("enter a choice(rock,scissors,paper): ")
-  com_choice = "scissors"
+  options=["rock","scissors","paper"]
+  com_choice = random.choice(options)
   choices = {"player" : player_choice, "computer":com_choice}
   return choices
 
