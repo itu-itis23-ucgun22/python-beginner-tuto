5edf5347a8d160f2aab95624abf2f54adbce86ec
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 04:39:35 +0300

    input function

--- python_lessons/main.py
+++ python_lessons/main.py
@@ -1,5 +1,5 @@
 def get_choices():
-  player_choice = "rock"
+  player_choice = input("enter a choice(rock,scissors,paper): ")
   com_choice = "scissors"
   choices = {"player" : player_choice, "computer":com_choice}
   return choices
