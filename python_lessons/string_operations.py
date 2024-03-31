bc1c69235e094397c30a29baaeba399d9d3118d8
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 07:25:05 +0300

    string operations

---
+++ python_lessons/string_operations.py
@@ -0,0 +1,23 @@
+name ="Mustafa Orçun"
+name +=" Uçgun"
+life = "gone"
+print("""Mustafa Orçun
+      
+      
+      Uçgun""")
+
+print(life.isalpha())
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isalnum())
+print(name.title())
+print(name.endswith("Mustafa Orçun Uçgun"))
+print(name.startswith("M"))
+print(name.split("u"))
+print(name.replace(name,"mou"))
+print(name.strip("Mn"))
+print(name.join(['kral','mou']))
+print(name.find("u"))
+print(len(name))
+print("s o" in name)
