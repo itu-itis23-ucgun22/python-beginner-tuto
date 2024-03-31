4db20daf67701b113da4226e01551112db55c175
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 11:00:14 +0300

    Escaping Characters

---
+++ python_lessons/escaping_characters.py
@@ -0,0 +1,12 @@
+name1 ="MOU"
+name2 = "M'O'U"
+name3 = 'MOU""'
+name4 = "MO\"u"
+name5 = "MO\\U"
+name6 = "MO\\\"U"
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(name5)
+print(name6)
