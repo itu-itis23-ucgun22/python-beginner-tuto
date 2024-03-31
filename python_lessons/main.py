087ff4db60d48947aa417b840f3145de7ebfbb94
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 05:57:36 +0300

    else and elif statements

--- python_lessons/main.py
+++ python_lessons/main.py
@@ -11,5 +11,20 @@ def check_win(player,computer):
   print(f"You chose {player} computer chose {computer}")
   if player == computer:
     return "It's a tie"
+  elif player == "rock" and computer == "scissors":
+    return "You win"
+  elif player == "scissors" and computer == "paper":
+    return "You win"
+  elif player == "scissors" and computer == "rock":
+    return "You lose"
+  elif player == "rock" and computer == "paper":
+    return "You lose"
+  elif player == "paper" and computer == "scissors":
+    return "you lose"
+  elif player == "paper" and computer == "rock ":
+    return "you win"
+  else:
+    return "invalid choice"
+
 
 check_win("rock","paper")
