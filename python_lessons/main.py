0d4e60777865f43b5f0e28dbd1918294442e5caf
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 06:28:11 +0300

    data types

--- python_lessons/main.py
+++ python_lessons/main.py
@@ -1,32 +1,15 @@
-import random 
+name = "MOU"
+print(type(name)==str)
 
-def get_choices():
-  player_choice = input("enter one choice (rock,scissors,paper): ")
-  options=["rock","scissors","paper"]
-  com_choice = random.choice(options)
-  choices = {"player" : player_choice, "computer":com_choice}
-  return choices
+age = 20
+print(isinstance(age,int))
 
-def check_win(player,computer):
-  print(f"You chose {player} computer chose {computer}")
-  if player == computer:
-    return "It's a tie"
-  elif player == "rock":
-    if computer == "paper":
-      return "you lose"
-    else: 
-      return "you win"
-  elif player == "paper":
-    if computer == "scissors":
-      return "you lose"
-    else: 
-      return "you win"
-  elif player == "scissors":
-    if computer == "rock":
-      return "you lose"
-    else: 
-      return "you win"
+height = 1.74
+print(isinstance(height,float))
 
-choices = get_choices()
-result = check_win(choices["player"],choices["computer"])
-print(result)
+kilo = float(75)
+print(kilo) 
+print(isinstance(kilo,float))
+
+
+#when statement print(kilo + isinstance(kilo,float)) the output was 76.0 so it takes the boolean value as 1
