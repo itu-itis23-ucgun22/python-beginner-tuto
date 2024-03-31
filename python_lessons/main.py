60311ab82fd53adc90bcd131f934fd3a8c7ef9c1
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-03-31 05:33:44 +0300

    f strings

--- python_lessons/main.py
+++ python_lessons/main.py
@@ -8,6 +8,8 @@ def get_choices():
   return choices
 
 def check_win(player,computer):
-  print("You chose" + player + "computer chose" + computer)
+  print(f"You chose {player} computer chose {computer}")
   if player == computer:
     return "It's a tie"
+
+check_win("rock","paper")
