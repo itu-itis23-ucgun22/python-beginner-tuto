4d1cbe391369a6dc8d17562a1685e1f1abfd8d7d
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-04-02 09:45:29 +0300

    modules

---
+++ python_lessons/modules.py
@@ -0,0 +1,17 @@
+from math import sqrt
+from re import search
+
+print(sqrt(4324532))
+sıra = print(search("MOU","Merhaba ben MOU ( is the king )"))
+print(sıra)
+# math for math utilities
+# re for regular expression
+# json to work with json
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for operating system utilities
+# random for random number generation
+# statistic for statistic utilities
+# request to perfrom hTTP network requests
+# htttp to create http servers
+# urllib to manage to urls
