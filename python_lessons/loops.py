d4d77ebbe8aa664cb985b58d7facf973a8254ef3
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-04-02 08:12:38 +0300

    break and continue

--- python_lessons/loops.py
+++ python_lessons/loops.py
@@ -1,10 +1,23 @@
 condition = True
 count = 0
-
-while condition == True & count < 5:
+print(count)
+while condition == True and count < 5:
     print("it is true")
-    count +=1
+    count += 1
+    
+print(count)
 
 items = [234,435,654,975,243]
 for index,item in enumerate(items):
-    print(item,index)
+   
+   if item == 435 or index == 4:
+        continue
+   print(item,index)
+numbers = [28,508299,340803,2893]
+for a , number in enumerate(numbers):
+    if a == 1:
+        break
+    print(a,number)
+    
+for x in range(6,15):
+    print(x)
