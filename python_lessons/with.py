3390ae4ba33a394537577e9d58deb943ea930bec
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-04-02 11:51:22 +0300

    with

---
+++ python_lessons/with.py
@@ -0,0 +1,7 @@
+filename = '/Users/mustafaorcunucgun/Documents/dersler/python-beginner-tuto/python_lessons/tuples.py'
+
+#
+
+with open(filename,'r') as file:
+    content = file.read()
+    print(content)
