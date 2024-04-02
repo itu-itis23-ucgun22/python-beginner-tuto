70e28075ba7605ef1b68c8a4da27aaca89eca650
itu-itis23-ucgun22 <ucgun22@itu.edu.tr>
2024-04-02 07:52:54 +0300

    loops

---
+++ python_lessons/loops.py
@@ -0,0 +1,10 @@
+condition = True
+count = 0
+
+while condition == True & count < 5:
+    print("it is true")
+    count +=1
+
+items = [234,435,654,975,243]
+for index,item in enumerate(items):
+    print(item,index)
